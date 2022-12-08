dc03cb80ae12c6309479b7011f4a077e292f8a32
matsudakazuki887 <119277846+matsudakazuki887@users.noreply.github.com>
2022-12-08 13:24:23 +0900

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -3,12 +3,13 @@
 
 int calendar_jg(int year, int month);
 bool uru(int year, int month);
+int mousejg(int month, int h);
+int calendar_x1 = 0, calendar_y1 = 80, calendar_x2 = 714, calendar_y2 = 450;
 void Main()
 {
 	// 背景色を ColorF{ 0.3, 0.6, 1.0 } に設定
 	Scene::SetBackground(Palette::White);
-	int i = 0, j = 1,h=0;
-	int calendar_x1 = 0, calendar_y1 = 80, calendar_x2 = 714, calendar_y2 = 450;
+	int i = 0, j = 1, h = 0;
 	String text = U"日月火水木金土";
 	const Date date = Date::Today();
 	h = calendar_jg(date.year, date.month);
@@ -29,9 +30,9 @@ void Main()
 		Print << U"Y: " << Cursor::Pos().y; // Y 座標だけを表示
 		Print << h; // Y 座標だけを表示
 		Rect{ 0, 0, calendar_x2, calendar_y1 }.draw(ColorF{ 0, 0, 0, 0.05 });
-		fontBold25(m,U"月").draw(300, 20, Palette::Black);
+		fontBold25(m, U"月").draw(300, 20, Palette::Black);
 		fontBold18(y).draw(370, 25, Palette::Silver);
-		Rect{ calendar_x1-1, calendar_y1, calendar_x2+2, calendar_y2 } //カレンダー出力　座標(x1,y1)を左上の基準にして幅x2,高さy2の長方形をかく //適当にやってるのは後で直す
+		Rect{ calendar_x1 - 1, calendar_y1, calendar_x2 + 2, calendar_y2 } //カレンダー出力　座標(x1,y1)を左上の基準にして幅x2,高さy2の長方形をかく //適当にやってるのは後で直す
 		.drawFrame(1, 0, Palette::Black);
 		for (i = 1; i <= 6; i++)
 		{
@@ -39,23 +40,26 @@ void Main()
 		}
 		for (i = 1; i <= 5; i++)
 		{
-			Line{ calendar_x1, calendar_y2 / 5 * i + calendar_y1 , calendar_x2 + calendar_x1 ,calendar_y2 / 5 * i + calendar_y1 }.draw(0.5, Palette::Silver); //横の線
+			Line{ calendar_x1, calendar_y2 / 6 * i + calendar_y1 , calendar_x2 + calendar_x1 ,calendar_y2 / 6 * i + calendar_y1 }.draw(0.5, Palette::Silver); //横の線
 		}
 		if (uru(y, m))month[2]++;
 		for (i = 1; i <= month[m]; i++)
 		{
-			fontBold(i).draw(calendar_x2 / 7 * ((i+h - 1) % 7) + calendar_x1+4, calendar_y2/5 * j + calendar_y1+2, Palette::Black); //日付
-			if ((i+h) % 7 == 0)j++;
+			fontBold(i).draw(calendar_x2 / 7 * ((i + h - 1) % 7) + calendar_x1 + 4, calendar_y2 / 6 * j + calendar_y1 + 2, Palette::Black); //日付
+			if ((i + h) % 7 == 0)j++;
 		}
 		j = 0;
 		if (uru(y, m))month[2]--;
-		for (i = 1; i <=7; i++)
+		for (i = 1; i <= 7; i++)
 		{
-			if(i==1) fontBold(text[i - 1]).draw(calendar_x2 / 7 * i + calendar_x1 - 57, calendar_y1 - 20, Palette::Red);
-			else if(i==7) fontBold(text[i - 1]).draw(calendar_x2 / 7 * i + calendar_x1 - 57, calendar_y1 - 20, Palette::Blue);
-			else fontBold(text[i - 1]).draw(calendar_x2 / 7 * i + calendar_x1-57, calendar_y1 - 20, Palette::Black);
+			if (i == 1) fontBold(text[i - 1]).draw(calendar_x2 / 7 * i + calendar_x1 - 57, calendar_y1 - 20, Palette::Red); // 曜日表示
+			else if (i == 7) fontBold(text[i - 1]).draw(calendar_x2 / 7 * i + calendar_x1 - 57, calendar_y1 - 20, Palette::Blue);
+			else fontBold(text[i - 1]).draw(calendar_x2 / 7 * i + calendar_x1 - 57, calendar_y1 - 20, Palette::Black);
 		}
-		
+		if (MouseL.down())
+		{
+			mousejg(month[m], h);
+		}
 		if (KeyLeft.down())
 		{
 			m--;
@@ -121,5 +125,19 @@ bool uru(int year, int month)
 		else {
 			return true;
 		}
+	}
+}
+int mousejg(int month,int h)
+{
+	int i = 0,j=0,x,y;
+	x = Cursor::Pos().x;
+	y = Cursor::Pos().y;
+	for (i = 1; i <= month; i++)
+	{
+		if (x>calendar_x1+(calendar_x2/7)* ((i + h - 1) % 7) && x<=calendar_x1+(calendar_x2/7)* ((i + h - 1) % 7+1) && y>calendar_y1+ (calendar_y2/6)*j && y<= calendar_y1 + (calendar_y2 / 6) * (j+1))
+		{
+			return i;
+		}
+		if ((i + h) % 7 == 0)j++;
 	}
 }
